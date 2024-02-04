80925391e2e794b6dfabc7f5d9969686ef304a09
Kamil <kamilggss@gmail.com>
2024-02-04 19:22:51 +0300

    Added custom func's to templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,41 +4,50 @@ import (
 	"html/template"
 	"path/filepath"
 	"snippetbox/internal/models"
+	"time"
 )
+
 type templateData struct {
-	Snippet *models.Snippet
-	Snippets []*models.Snippet
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
 	CurrentYear int
 }
 
+func humanDate(t time.Time) string {
+	return t.Format(time.DateTime)
+}
 
-func newTemplateCache() (map[string]*template.Template, error){
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
 
 	cache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
-	for _, page := range pages{
+	for _, page := range pages {
 		name := filepath.Base(page)
 
-	
-		ts, err := template.ParseFiles(filepath.Join("./ui/html/base.tmpl.html"))
-		if err != nil{
+		ts := template.New(name).Funcs(functions)
+
+		ts, err := ts.ParseFiles(filepath.Join("./ui/html/base.tmpl.html"))
+		if err != nil {
 			return nil, err
 		}
 		ts, err = ts.ParseGlob(filepath.Join("./ui/html/partials/*.tmpl.html"))
-		if err != nil{
+		if err != nil {
 			return nil, err
 		}
 
 		ts, err = ts.ParseFiles(filepath.Join(page))
-		if err != nil{
+		if err != nil {
 			return nil, err
 		}
-	
 
 		cache[name] = ts
 	}
